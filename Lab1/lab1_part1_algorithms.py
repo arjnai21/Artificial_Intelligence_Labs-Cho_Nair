a1cb549c44a8bbb6fbcc58e9777bbf44cbcf0bf3
ArjunNair03 <arjun@nairsnet.com>
2019-04-05 23:05:29 -0400

    generate_next_states and some other stuff

--- Lab1/lab1_part1_algorithms.py
+++ Lab1/lab1_part1_algorithms.py
@@ -91,7 +91,6 @@ def RandWalk(initial_state, avoid_backtrack = False, filtering = False, cutoff =
 
     # if loop breaks before finding goal, search is failure; return None
     return None
-# yurt
 
 """
 Implement DFS, BFS, and UCS algorithms in a similar paradigm to the above.
@@ -156,7 +155,6 @@ def DFS(initial_state, avoid_backtrack = False, filtering = False, cutoff = INF,
     # if loop breaks before finding goal, search is failure; return None
     return None
 
-#yurt
 def BFS(initial_state, avoid_backtrack = False, filtering = False, cutoff = INF,
         state_callback_fn = lambda state : False, # A callback function. If it returns True, terminate
         counter = {'num_enqueues':0, 'num_extends':0}):

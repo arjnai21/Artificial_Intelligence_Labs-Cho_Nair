3176f736c00f581d4049bac7a6e8c9ff1c1a782f
Elias <elicho21@bergen.org>
2019-05-03 15:35:22 -0400

    Updating most of the files

--- Lab2/lab2_test_gui.py
+++ Lab2/lab2_test_gui.py
@@ -53,7 +53,7 @@ ALGORITHMS =  {"0) Random Policy" : RandChoice,
 STEP_TIME_OPTIONS = (0.00, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,
                     0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 2.0, 5.0)
 
-CUTOFF_OPTIONS = tuple( ['INF'] + list(range(1,100)))
+CUTOFF_OPTIONS = tuple( ['INF'] + list(range(0,100)))
 
 INITIAL_WAITING = 0
 SEARCHING_RUNNING = 1
